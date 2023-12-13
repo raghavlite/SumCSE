d67f6819f46df9e945b9b01cb043813366af156f
raghavlite <raghavlite@gmail.com>
2023-12-13 17:17:18 -0500

    added scripts

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,7 +1,10 @@
-path=sjtu-lit/SynCSE-partial-RoBERTa-base
+#!/usr/bin/env bash
+# path=sjtu-lit/SynCSE-partial-RoBERTa-base
+path=${1}
+# path=result/my-sup-simcse-roberta-base_sjtu-lit/SynCSE-partial-NLI
 python simcse_to_huggingface.py --path ${path}
 CUDA_VISIBLE_DEVICES=0 python evaluation.py \
     --model_name_or_path ${path} \
     --pooler cls \
-    --task_set sts \
+    --task_set full \
     --mode test
